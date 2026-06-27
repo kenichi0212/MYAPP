6af12239dc247778241784fea5ff30f6da425520
HARUYUME\kfhy7 <siid.kt.0212@gmail.com>
2026-06-28 08:58:14 +0900

    Trust all proxies so HTTPS scheme is detected behind Render's load balancer
    RenderはTLSをロードバランサーで終端しHTTPでコンテナに転送するため、
    trustProxiesを設定しないとasset()等がhttpの絶対URLを生成し、
    HTTPS配信時にCSS/JSがMixed Contentとしてブロックされる。

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // RenderはTLSをロードバランサー側で終端しHTTPでコンテナへ転送するため、
+        // X-Forwarded-Protoを信頼しないとasset()等がhttpの絶対URLを生成してしまう。
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
